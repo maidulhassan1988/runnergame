5d3646ee0e73c08243b2eeb0c7ca4d37e0f873f8
maidul <maidulhassan@gmail.com>
2023-05-02 23:48:04 +0600

    introduction of player one

--- game.py
+++ game.py
@@ -16,6 +16,8 @@ text_surface = test_font.render('My game', False, 'black')
 snail_surface = pygame.image.load('graphics/snail/snail.png').convert_alpha()
 snail_x_pos = 600
 
+player_surface = pygame.image.load('graphics/player/player1.png').convert_alpha()
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -28,8 +30,7 @@ while True:
 	snail_x_pos -= 1
 	if snail_x_pos < -100: snail_x_pos = 800
 	screen.blit(snail_surface, (snail_x_pos,270))
+	screen.blit(player_surface, (80,200))
 
 	pygame.display.update()	
 	clock.tick(60)
-
-
